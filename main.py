55f1bdc2f6e30e126110b977db77f9cd1c029510
Stig Palmquist <stig@stig.io>
2018-09-11 22:23:30 +0200

    woops

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
 sensor = tmp102.Tmp102(i2c, 0x48)
 
 while True:
-    print("temp: sensor.temperature")
+    print("temp: " + str(sensor.temperature))
     time.sleep_ms(3000)
